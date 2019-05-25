85f5c3b2cd832cfbcdd86db56364326477ffac5a
Ray <raymundo170@hotmail.com>
2019-05-24 20:39:26 -0500

    Configuración app hospital

---
+++ app/src/main/java/inventarios/uv/mx/apphospital/App.kt
@@ -0,0 +1,9 @@
+package inventarios.uv.mx.apphospital
+
+import androidx.multidex.MultiDexApplication
+
+class App: MultiDexApplication() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
